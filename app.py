472642e7807e8e21fb64fef7f99a1a899df43549
Timotej Ružička <timotej.ruzicka@gmail.com>
2023-10-06 15:26:23 +0200

    Add prices and co2 based on country

--- app.py
+++ app.py
@@ -11,9 +11,13 @@ with open('light_bulbs.csv', newline='') as csvfile:
 
 countries_co2  = {}
 countries = []
+countries_codes =  {}
 with open('co2.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
+        if not row["Country"] in countries:
+            countries.append(row["Country"])
+            countries_codes[row["Country"]] = row["Code"]
         countries_co2[row["Country"]] = float(row["CO2"])/1000
 kwh_prices = {}
 
@@ -21,7 +25,7 @@ with open('kWh_price.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         add = {row["Code"], row["KWH_price"]}
-        kwh_prices[row["Code"]] = row["KWH_price"]
+        kwh_prices[row["Code"]] = float(row["KWH_price"])
 
 app = Flask(__name__)
 
@@ -36,26 +40,29 @@ def calc():
     
     if request.method == "POST":
         # Check if all fields are filled
-        variables = ["price0", "price1", "watts0", "watts1", "amount", "average", "type0", "type1"]
+        variables = ["price0", "price1", "watts0", "watts1", "amount", "average", "type0", "type1", "country"]
         if not all(request.form.get(field) for field in variables):
             return render_template("error.html", error="Please fill out all the fields.")
         # Def variables
         s = "s" # If its one year or more years
-        # TODO: add prices based on region
-        kwh_price = 0.15 # Price for kwh
+        country = request.form.get("country")
+        try:
+            kwh_price = kwh_prices[countries_codes[country]]
+        except KeyError:
+            kwh_price = 0.15
         average = int(request.form.get("average"))
         amount = int(request.form.get("amount"))
         watts = []
         price = []
         type = []
-
+        # Read values from input
         for i in range(2):
             watts.append(int(request.form.get(f"watts{i}")))
             price.append(int(request.form.get(f"price{i}")))
             type.append(request.form.get(f"type{i}"))
-        # TODO: chainge co2 based on region
-        kwh_co2 = 0.5
+        kwh_co2 = countries_co2[country]
         co2_manu = []
+        # CO2 manu for first and second lightbulb
         for j in range (2):
             for i in range (0, len(light_bulbs)):
                 if light_bulbs[i]["name"] == type[j]:
@@ -77,8 +84,7 @@ def calc():
             years="∞"
         co2 = [] # Co2 of new product
         for i in range (10):
-            # TODO: co2_manu
-            value = round((watts[1]/1000*amount*average*365*i+co2_manu[1]*kwh_co2)-(watts[0]/1000*amount*average*kwh_co2*365*i+co2_manu[0]),2)
+            value = round((watts[1]/1000*amount*average*365*i*kwh_co2+co2_manu[1])-(watts[0]/1000*amount*average*kwh_co2*365*i+co2_manu[0]),2)
             co2.append(value)
         return render_template("resoult.html", years=years, s=s, values=profit_values, co2=co2)
 
